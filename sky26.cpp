4c9b23aa400bfdb98200e9d4465914b4843c33a7
Raushan-Shahi <raushanshahi2001@gmail.com>
2023-03-28 21:35:48 +0530

    nested for

--- sky26.cpp
+++ sky26.cpp
@@ -1,25 +1,27 @@
-//array in a for loop 
-// for inside a for 
-
+// array in a for loop
+//  for inside a for
 
 #include <iostream>
 using namespace std;
- 
-int main(){
-    int i , j ;
-    char  k[4] ;
-    int m[3] ; 
 
-for (int i=0; i<4 ; i++) {
-cout << " enter the name of kid:  " << i+1 << endl;
-cin >> k[i];
-        cout << "enter the marks of kid:  "<<i+1 << endl;
-    for (j=0; j<3 ; j++){
-        cout << "marks in sublect: "<<j+1 << endl;
-        cin >> m[j] ;
+int main()
+{
+    int i, j;
+    char k[4];
+    int m[3];
+
+    for (int i = 0; i < 4; i++)
+    {
+        cout << " enter the name of kid:  " << i + 1 << endl;
+        cin >> k[i];
+        cout << "enter the marks of kid:  " << i + 1 << endl;
+        for (j = 0; j < 3; j++)
+        {
+            cout << "marks in sublect: " << j + 1 << endl;
+            cin >> m[j];
+        }
+        cout << "the total marks of kid: " << i + 1 << " is " << m[0] + m[1] + m[2] << endl;
     }
-    cout << "the total marks of kid: "<< i+1 << " is "<<m[0]+m[1]+m[2]<<endl; 
-}
 
-return 0;
+    return 0;
 }
