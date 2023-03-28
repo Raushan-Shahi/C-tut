877279f55a1a8a68a52f2cec3a2cd4e3a61a91e9
Raushan-Shahi <raushanshahi2001@gmail.com>
2023-03-28 21:13:25 +0530

    pointers

--- sky24.cpp
+++ sky24.cpp
@@ -1,17 +1,18 @@
-// pointers 
-// it is the address of a data 
+// pointers
+// it is the address of a data
 
 #include <iostream>
 using namespace std;
- 
-int main(){
-int a =3 ;
-int* b = &a;
-int** c = &b ;
-cout << " the value of a is "<< a << endl;
-cout << "the address of a is "<< b << endl;
-cout << "the address of b is " << c << endl;
-cout << " the content in c is "<< **c<< endl;
 
-return 0;
+int main()
+{
+    int a = 3;
+    int *b = &a;
+    int **c = &b;
+    cout << " the value of a is " << a << endl;
+    cout << "the address of a is " << b << endl;
+    cout << "the address of b is " << c << endl;
+    cout << " the content in c is " << **c << endl;
+
+    return 0;
 }
