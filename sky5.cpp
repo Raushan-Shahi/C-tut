7a6a964f079fd8b52c8423c6c1bb97fa7f90b38b
Raushan-Shahi <raushanshahi2001@gmail.com>
2023-03-28 18:10:53 +0530

    code edit

--- sky5.cpp
+++ sky5.cpp
@@ -5,16 +5,16 @@
 using namespace std;
 
 int main() {
-// char ram = 'a' ;
-// // float m = 1.456 ;
-// //double g = 1.208677455 ;
-// //cout << g ;
-// // cout << m ;
-// bool w = true ;
-// bool e = false ;
-// cout << ram<<endl ;
-// cout << w<<endl ;
-// cout << e<<endl ; 
+char ram = 'a' ;
+float m = 1.456 ;
+double g = 1.208677455 ;            // in case of double the output is upto 5 decimal places by default 
+cout << g <<endl;
+cout << m <<endl;
+bool w = true ;
+bool e = false ;
+cout << ram<<endl ;
+cout << w<<endl ;
+cout << e<<endl ; 
 
 cout<<5%11<<endl;
     return 0;
